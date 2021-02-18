80d8032fc3e4a2474309a9b11fb1739c0075b990
Joe <jackwmj12@163.com>
2021-02-18 21:57:19 +0800

    1.2.4 .临时推送版本

---
+++ demo/base_redis.py
@@ -0,0 +1,54 @@
+
+import os
+
+from dbentrust.main import installRedis
+
+installRedis()
+
+from dbentrust.memobject import *
+
+class User(MemAdmin):
+    _tablename_ = "user"
+
+    def __init__(self, pk=None):
+        super(User, self).__init__(pk)
+        
+        self.username = ""
+
+    def keys(self):
+        return ("username",)
+    
+
+class Book(MemObject):
+    _tablename_ = "books:book"
+    
+    def __init__(self, pk=None):
+        super(Book, self).__init__(pk)
+        
+        self.bookname = ""
+    
+    def keys(self):
+        return ("bookname",)
+    
+class UserBooks(MemRelation):
+    _tablename_ = "books"
+    
+    _root_ = User
+    _leafs_ =[Book]
+
+
+if __name__ == '__main__':
+    config = {
+        "REDIS_HOST": os.environ.get("HOST_OF_TEST"),
+        "REDIS_PASSWORD": os.environ.get("PASSWORD_OF_REDIS"),
+    }
+    MemConnectionManager.initConnection(config)
+    user = User(1)
+    user.username = "joe"
+    user.insert_without_sync()
+    book = Book(1)
+    book.bookname = "joe's first book"
+    book.insert_without_sync()
+    books = user.build_empty_relation(UserBooks)
+    books.append(book)
+    print(user.get_all())
